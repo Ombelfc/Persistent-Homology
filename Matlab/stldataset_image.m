a8e480f26880548aa0565438565fd52e3848dac6
Omar <berraj97@gmail.com>
2018-06-19 21:44:29 +0200

    Adding report

---
+++ Matlab/stldataset_image.m
@@ -0,0 +1,43 @@
+
+clc; clear; close all;
+import edu.stanford.math.plex4.*;
+
+load test.mat
+A = im2double(X(:, 1:80));
+size(A)
+
+max_dimension = 3;
+num_landmark_points = 50;
+nu = 1;
+num_divisions = 1000;
+
+% create a sequential maxmin landmark selector
+landmark_selector = api.Plex4.createMaxMinSelector(A, num_landmark_points);
+R = landmark_selector.getMaxDistanceFromPointsToLandmarks()
+max_filtration_value = R / 3;
+
+% create a lazy witness stream
+stream = streams.impl.LazyWitnessStream(landmark_selector.getUnderlyingMetricSpace(), landmark_selector, max_dimension, max_filtration_value, nu, num_divisions);
+stream.finalizeStream()
+
+% print out the size of the stream - will be quite large since the complex
+% construction is very sensitive to the maximum filtration value
+num_simplices = stream.getSize()
+
+% get persistence algorithm over Z/2Z
+persistence = api.Plex4.getModularSimplicialAlgorithm(max_dimension, 2);
+
+% compute the intervals
+intervals = persistence.computeIntervals(stream);
+
+% create the barcode plots
+options.filename = 'lazyRange';
+options.max_filtration_value = max_filtration_value;
+options.max_dimension = max_dimension - 1;
+plot_barcodes(intervals, options);
+
+%% DCT
+
+figure;
+scatter(A(:,1), A(:,50), '.')
+axis square
